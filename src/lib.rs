959a29dc49c946ffaa29e0c563e5ef9fac3881ed
wkg <wkg@williamgasper.com>
2020-07-25 17:36:51 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub fn this_function(x: i8) -> i8 {
+    x + 5
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn this_function_0() {
+        assert_eq!(this_function(4), 9);
+    }
+}
